3f10a2d0223ce40308ed88ce2db13c2258fbf621
imusa654 <imusa@nibss-plc.com.ng>
2025-08-07 17:11:14 +0100

    Update CORS to allow all origins

---
+++ src/main/java/com/musa/fraud_detection_backend/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.musa.fraud_detection_backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Allow all origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(false); // Set to false when using allowedOrigins("*")
+    }
+}
