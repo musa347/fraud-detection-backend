7db8c0bc682d91a9e9db70cd9190020e9bd8429d
imusa654 <imusa@nibss-plc.com.ng>
2025-08-25 08:58:32 +0100

    testing

--- src/main/java/com/musa/fraud_detection_backend/service/FraudScoringService.java
+++ src/main/java/com/musa/fraud_detection_backend/service/FraudScoringService.java
@@ -3,7 +3,6 @@ package com.musa.fraud_detection_backend.service;
 import com.musa.fraud_detection_backend.dto.FraudResponse;
 import com.musa.fraud_detection_backend.dto.TransactionDto;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -12,11 +11,9 @@ public class FraudScoringService {
 
     private final WebClient webClient;
 
-    public FraudScoringService(@Value("${fraud.ml.url}") String mlUrl) {
-        this.webClient = WebClient.builder()
-                .baseUrl(mlUrl)
-                .defaultHeader(MediaType.APPLICATION_JSON_VALUE)
-                .build();
+    public FraudScoringService(@Value("${fraud.ml.url}") String mlurl) {
+        System.out.println("mlUrl: " + mlurl);
+        this.webClient = WebClient.create(mlurl);
     }
 
     public FraudResponse scoreTransaction(TransactionDto tx) {
